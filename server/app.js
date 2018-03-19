2e22ccaaebb913e33aab6fa76f000977984fa5fd
Arnau Diaz <diazarnau0@gmail.com>
2018-03-19 15:56:28 +0100

    Render basic html in server side

--- server/app.js
+++ server/app.js
@@ -8,9 +8,9 @@ const indexRoutes = require('./routes/index');
 const app = express();
 
 // settings
-// app.set('views', path.join(__dirname, 'views'));
-// app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 app.set('port', process.env.PORT || 3000);
 

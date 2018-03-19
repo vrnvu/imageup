ed060ed1c549d923122820d5d7000589d0a20681
Arnau Diaz <diazarnau0@gmail.com>
2018-03-19 15:54:02 +0100

    Routing added

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
-//const indexRoutes = require('./routes/index');
+const indexRoutes = require('./routes/index');
 //const tasksRoutes = require('./routes/tasks');
 
 const app = express();
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 // routes
-// app.use('/', indexRoutes);
+ app.use('/', indexRoutes);
 //app.use('/api', tasksRoutes);
 
 // static files
